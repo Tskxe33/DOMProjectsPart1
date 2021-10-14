8e195a1845f2ac8856801ded57488df6fe1d6bb5
Petro Arva <petroarva33@gmail.com>
2021-10-14 23:42:09 +0200

    Added style and cookie for counting saved contact

--- NewContact/information.js
+++ NewContact/information.js
@@ -1,15 +1,19 @@
 const query = new URLSearchParams(location.search);
-let FirstName = query.get('Name');
-if(FirstName){
-    FirstName = FirstName.trim();
+let FirstName = query.get("Name");
+if (FirstName) {
+  FirstName = FirstName.trim();
 }
-const dob = query.get('Dob');
-const surname = query.get('Surname');
+const dob = query.get("Dob");
+const surname = query.get("Surname");
+const numOfSave = query.get("numOfSave");
+console.log(surname, numOfSave);
 
-const message = `
-First Name: ${FirstName},
-Surname: ${surname},
-Date of Birth: ${dob}
-`
+document.querySelector(".info__name").textContent = `First Name: ${FirstName}`;
 
-document.querySelector('.form__message').innerHTML = message;
+document.querySelector(".info__surname").textContent = `Surname: ${surname}`;
+
+document.querySelector(".info__dob").textContent = `Date of Birth: ${dob}`;
+
+document.querySelector(
+  ".info__numOfSave"
+).textContent = `Number of Saved Contact: ${numOfSave}`;
--- NewContact/script.js
+++ NewContact/script.js
@@ -2,37 +2,46 @@
 
 const form = document.querySelector(".form");
 
+function saveCookie(name, value) {
+  document.cookie = `${name}=${value}`;
+}
+
+console.log(+document.cookie.split("=")[1]);
+
 form.addEventListener("submit", function (event) {
   event.preventDefault();
+  let cookieValue = +document.cookie.split("=")[1];
+  console.log(cookieValue);
+  saveCookie(`count`, ++cookieValue);
+
   const formElements = event.target.elements;
   const query = new URLSearchParams();
-  
-  query.set('Name', `${formElements[`name`].value}`)
-  query.set('Surname', `${formElements[`surname`].value}`)
-  query.set('Dob', `${formElements[`dob`].value}`)
-  
-  location = `information.html?${query}`
+  query.set("Name", `${formElements[`name`].value}`);
+  query.set("Surname", `${formElements[`surname`].value}`);
+  query.set("Dob", `${formElements[`dob`].value}`);
+  query.set("numOfSave", `${cookieValue}`);
+
+  location = `information.html?${query}`;
 });
 
-
-function browserlanguage(){
-  const name = document.querySelector('.name');
-  const surname = document.querySelector('.surname');
-  const dob = document.querySelector('.dob');
-  const btn = document.querySelector('.btn');
+function browserlanguage() {
+  const name = document.querySelector(".name");
+  const surname = document.querySelector(".surname");
+  const dob = document.querySelector(".dob");
+  const btn = document.querySelector(".btn");
   const language = navigator.language;
-  if(language === `en-US`) {
-    name.textContent = `Name:`
-    surname.textContent = `Surname:`
-    dob.textContent = `Date of birth:`
-    btn.textContent = `Save`
+  if (language === `en-US`) {
+    name.textContent = `Name:`;
+    surname.textContent = `Surname:`;
+    dob.textContent = `Date of birth:`;
+    btn.textContent = `Save`;
   }
 
-  if(language === 'sr') {
-    name.textContent = `Ime:`
-    surname.textContent = `Prezime:`
-    dob.textContent = `Datum Rodjenja:`
-    btn.textContent = `Sacuvaj`
+  if (language === "sr") {
+    name.textContent = `Ime:`;
+    surname.textContent = `Prezime:`;
+    dob.textContent = `Datum Rodjenja:`;
+    btn.textContent = `Sacuvaj`;
   }
 }
 
