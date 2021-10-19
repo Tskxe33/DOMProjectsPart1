d3552cd1931c48bdce137465a04de65e8b0e1b54
Petro Arva <petroarva33@gmail.com>
2021-10-19 18:54:39 +0200

    Created module cookies

---
+++ NewContact/modules/cookies.js
@@ -0,0 +1,18 @@
+export function saveCookie(name, value) {
+  document.cookie = `${name}=${value}`;
+}
+
+export function getCookieValue(cookieName) {
+  let cookie = document.cookie
+    .split("; ")
+    .find((name) => name.startsWith(cookieName))
+    .split("=")[1];
+
+  return cookie;
+}
+
+export function cookieCountIncrese(cookieName) {
+  let count = getCookieValue(cookieName) || 0;
+  count++;
+  return count;
+}
