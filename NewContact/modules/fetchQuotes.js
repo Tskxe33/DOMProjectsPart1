2ea1843ba7fa354919160909d30edfa456d128c8
Petro Arva <petroarva33@gmail.com>
2021-10-20 11:05:47 +0200

    Moved getRandomQuote function from quotes module to script

--- NewContact/modules/fetchQuotes.js
+++ NewContact/modules/fetchQuotes.js
@@ -1,28 +1,42 @@
 function fetchQuotes() {
   const quotesURL = "https://type.fit/api/quotes";
+  const loader = document.querySelector(".loader");
+  loader.classList.remove("hidden");
   return fetch(quotesURL).then((response) => response.json());
 }
 
+// promeniti naziv scripte
 export function getRandomQuote() {
   return fetchQuotes().then((res) => {
-    const loader = document.querySelector(".loader");
-    loader.classList.remove("hidden");
+    // const loader = document.querySelector(".loader"); //izbaciti loader
+    // loader.classList.remove("hidden");
     const random = Math.trunc(Math.random() * res.length);
     return res.filter((quote, index) => (index === random ? quote : ""));
   });
 }
 
-setTimeout(function () {
-  const loader = document.querySelector(".loader");
+// setTimeout(function () {
+//   // izbaciti timeout
+//   getRandomQuote().then((res) => {
+//     const loader = document.querySelector(".loader");
 
-  getRandomQuote().then((res) => {
-    const quote = res[0];
-    displayQuote(quote.text, quote.author);
-    loader.classList.add("hidden");
-  });
-}, 2500);
+//     const quote = res[0];
+//     displayQuote(quote.text, quote.author);
+//     loader.classList.add("hidden");
+//   });
+// }, 0); // premestiti u script
 
-function displayQuote(quote, author) {
-  document.querySelector(".quotes__message").textContent = quote;
-  document.querySelector(".quotes__author").textContent = author;
-}
+// izbaciti timeout
+
+// getRandomQuote().then((res) => {
+//   const loader = document.querySelector(".loader");
+//   const quote = res[0];
+
+//   displayQuote(quote.text, quote.author);
+//   loader.classList.add("hidden");
+// });
+
+// function displayQuote(quote, author) {
+//   document.querySelector(".quotes__message").textContent = quote;
+//   document.querySelector(".quotes__author").textContent = author;
+// }
